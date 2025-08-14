9eeaf024503c32bb40bb319a87fa5d5437364d81
Nigel355 <ncalderon90918@ufide.ac.cr>
2025-08-13 18:08:48 -0600

    inicio

---
+++ EjercicioPractico2_Nigel/src/main/java/com/mycompany/ejerciciopractico2_nigel/EjercicioPractico2_Nigel.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejerciciopractico2_nigel;
+
+/**
+ *
+ * @author nigel
+ */
+public class EjercicioPractico2_Nigel {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
